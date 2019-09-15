a7c40a5f4dff3994837ebcbbce3b609e2e66df71
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-09-15 14:03:30 +0900

    加算と減算を実装

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-std=c11 -g -stati
+CFLAGS=-std=c11 -g -static
 
 xcc: xcc.c
 
--- xcc.c
+++ xcc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+
+    long char_long = strtol(p, &p, 10);
+    printf("    mov rax, %d\n", char_long);
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            char_long = strtol(p, &p, 10);
+            printf("    add rax, %ld\n", char_long);
+        } else if (*p == '-') {
+            p++;
+            char_long = strtol(p, &p, 10);
+            printf("    sub rax, %ld\n", char_long);
+        } else {
+            fprintf(stderr, "unexpected token: '%c'\n", *p);
+            return 1;
+        }
+    }
+
     printf("    ret\n");
     return 0;
 }
